d747ba28df03a1efe1d7f41659ac273a2a350290
candec <omnius.candec@gmail.com>
2022-09-23 13:10:06 +0100

    done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,1 +1,54 @@
 #include "serialization.hpp"
+
+#include <iostream>
+#include <stdint.h>
+#include <cstring>
+
+typedef struct s_Data
+{
+	std::string	name;
+	int		age;
+	bool	job;
+} Data;
+
+
+uintptr_t serialize(Data* ptr)
+{
+	uintptr_t raw  = (uintptr_t)ptr;
+
+	return (raw);
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	Data* ptr  = (Data*)raw;
+
+	return (ptr);
+}
+
+int main()
+{
+	Data person = {"John", 30, true};
+	uintptr_t raw;
+	Data *IveSeenThings;
+
+
+	std::cout << "Name:		" << person.name << std::endl
+			<< "Age:		" << person.age << std::endl
+			<< "Job:		" << person.job << std::endl;
+	
+	
+	raw = serialize(&person);
+
+	person.age += 5;
+	person.job = false;
+
+	IveSeenThings = deserialize(raw);
+
+
+	std::cout << "Name:		" << IveSeenThings->name << std::endl
+			<< "Age:		" << IveSeenThings->age << std::endl
+			<< "Job:		" << IveSeenThings->job << std::endl;
+	
+	return (0);
+}
